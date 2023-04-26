4639de0fbdfd6efdeb525f02931af14bcf357384
Brijesh <brijeshitaliya35@gmail.com>
2023-04-26 20:15:15 +0530

    dfdsf

--- index.js
+++ index.js
@@ -1,78 +1,24 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-// // const { mainRoutes } = require("./Routes/index");
-// const port = 3004;
+const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
-const cors = require("cors");
 
-// const dConnection =
-//     "mongodb+srv://admin:admin@cluster0.sshqcpg.mongodb.net/OnlineSeller";
+const app = express();
 
-// const options = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// };
-// mongoose
-//     .connect(dConnection, options)
-//     .then(() => {
-//         console.log("DB Connected!");
-//     })
-//     .catch((err) => {
-//         console.log("🚀 ~ file: index.js:24 ~ err:", err)
-//         throw new Error("Database credentials are invalid.");
-//     });
-
+// Connect to MongoDB database using Mongoose
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
-// app.use(cors());
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-// // Getting Request
-// app.get('/', (req, res) => {
- 
-//     // Sending the response
-//     res.send('Hello World!')
-
-//     // Ending the response
-//     res.end()
-// })
-
-// // app.use("/api/auth", authRoute);
-// // app.use("/onlineseller", mainRoutes);
 
 
 
-    
-// app.listen(port, () => {
-//     console.log("server started on", port);
-// });
 
-// module.exports = app
-
-
-const PORT = 4000
-
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
-
-app.use(cors());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
+// Define routes
 app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+  res.send('Hello, world!');
+});
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
-
-// Export the Express API
-module.exports = app
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
