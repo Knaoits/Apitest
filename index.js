91d18f4538e4c2e9e405a8b4e957875d964d0851
Brijesh <brijeshitaliya35@gmail.com>
2023-04-26 19:48:02 +0530

    s

--- index.js
+++ index.js
@@ -1,51 +1,71 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-// const { mainRoutes } = require("./Routes/index");
-const port = 3004;
-const cors = require("cors");
+// const express = require("express");
+// const app = express();
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// // const { mainRoutes } = require("./Routes/index");
+// const port = 3004;
+// const cors = require("cors");
 
-const dConnection =
-    "mongodb+srv://admin:admin@cluster0.sshqcpg.mongodb.net/OnlineSeller";
+// const dConnection =
+//     "mongodb+srv://admin:admin@cluster0.sshqcpg.mongodb.net/OnlineSeller";
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-};
-mongoose
-    .connect(dConnection, options)
-    .then(() => {
-        console.log("DB Connected!");
-    })
-    .catch((err) => {
-        console.log("🚀 ~ file: index.js:24 ~ err:", err)
-        throw new Error("Database credentials are invalid.");
-    });
+// const options = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// };
+// mongoose
+//     .connect(dConnection, options)
+//     .then(() => {
+//         console.log("DB Connected!");
+//     })
+//     .catch((err) => {
+//         console.log("🚀 ~ file: index.js:24 ~ err:", err)
+//         throw new Error("Database credentials are invalid.");
+//     });
 
-app.use(cors());
+// app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-// Getting Request
-app.get('/', (req, res) => {
+// // Getting Request
+// app.get('/', (req, res) => {
  
-    // Sending the response
-    res.send('Hello World!')
+//     // Sending the response
+//     res.send('Hello World!')
 
-    // Ending the response
-    res.end()
-})
+//     // Ending the response
+//     res.end()
+// })
 
-// app.use("/api/auth", authRoute);
-// app.use("/onlineseller", mainRoutes);
+// // app.use("/api/auth", authRoute);
+// // app.use("/onlineseller", mainRoutes);
 
 
 
     
-app.listen(port, () => {
-    console.log("server started on", port);
-});
+// app.listen(port, () => {
+//     console.log("server started on", port);
+// });
 
+// module.exports = app
+
+const express = require('express')
+
+const app = express()
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+// Export the Express API
 module.exports = app
