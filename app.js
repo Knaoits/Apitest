055635f63acbdc0dd91946df722b1acbfa1140b7
Brijesh <brijeshitaliya35@gmail.com>
2023-04-27 11:29:11 +0530

    dfdsf

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const PORT = 5000
 require('dotenv').config();
 const bodyParser = require("body-parser");
@@ -11,9 +11,9 @@ const cors = require("cors");
 console.log(process.env.MONGODB_URI);
 
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error(err));
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch(err => console.error(err));
 
 
   app.use(cors());
