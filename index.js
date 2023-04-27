ac6ed415f758bdc95bdcd7b54f311906c977a21a
Brijesh <brijeshitaliya35@gmail.com>
2023-04-27 10:14:14 +0530

    dfdsf

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 
 // Connect to MongoDB database using Mongoose
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error(err));
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch(err => console.error(err));
 
 
-  app.get('/favicon.ico', (req, res) => {
-    res.sendFile(path.join(__dirname, 'favicon.ico'));
-  });
+//   app.get('/favicon.ico', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'favicon.ico'));
+//   });
 
 
 
