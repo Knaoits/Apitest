58e8c2069c7aca9683826e68e1d3cf5686cc835b
Brijesh <brijeshitaliya35@gmail.com>
2023-04-27 12:07:47 +0530

    dfdsf

--- Controllers/index.js
+++ Controllers/index.js
@@ -2,7 +2,7 @@ module.exports = {
     Product:require('./Product'),
     Order:require('./Order'),
     Report : require('./Report'),
-    Seller : require('./seller'),
+    Seller : require('./Seller'),
     Buyer : require('./Buyer'),
     Admin : require('./Admin')
 };
