b2c5a5297340098a9cbfd7f9430e7273fbb22dc8
Brijesh <brijeshitaliya35@gmail.com>
2023-04-27 10:46:07 +0530

    dfdsf

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 
 // Connect to MongoDB database using Mongoose
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error(err));
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch(err => console.error(err));
 
 
 // Define routes
